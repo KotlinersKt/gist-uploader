68112a352453ccba67ecd658800acbf537ce2088
ChubyAvodroc <isc.ipn.david@gmail.com>
2020-10-27 01:33:41 -0600

    Fixing lint

--- src/main/kotlin/com/kotlinerskt/cli/giskt/github/GistResponse.kt
+++ src/main/kotlin/com/kotlinerskt/cli/giskt/github/GistResponse.kt
@@ -6,42 +6,42 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class GistResponse(
 
-		@SerialName("commits_url")
-		val commitsUrl: String? = null,
+    @SerialName("commits_url")
+    val commitsUrl: String? = null,
 
-		@SerialName("comments")
-		val comments: Int? = null,
+    @SerialName("comments")
+    val comments: Int? = null,
 
-		@SerialName("forks_url")
-		val forksUrl: String? = null,
+    @SerialName("forks_url")
+    val forksUrl: String? = null,
 
-		@SerialName("git_push_url")
-		val gitPushUrl: String? = null,
+    @SerialName("git_push_url")
+    val gitPushUrl: String? = null,
 
-		@SerialName("created_at")
-		val createdAt: String? = null,
+    @SerialName("created_at")
+    val createdAt: String? = null,
 
-		@SerialName("description")
-		val description: String? = null,
+    @SerialName("description")
+    val description: String? = null,
 
-		@SerialName("url")
-		val url: String? = null,
+    @SerialName("url")
+    val url: String? = null,
 
-		@SerialName("updated_at")
-		val updatedAt: String? = null,
+    @SerialName("updated_at")
+    val updatedAt: String? = null,
 
-		@SerialName("html_url")
-		val htmlUrl: String? = null,
+    @SerialName("html_url")
+    val htmlUrl: String? = null,
 
-		@SerialName("git_pull_url")
-		val gitPullUrl: String? = null,
+    @SerialName("git_pull_url")
+    val gitPullUrl: String? = null,
 
-		@SerialName("comments_url")
-		val commentsUrl: String? = null,
+    @SerialName("comments_url")
+    val commentsUrl: String? = null,
 
-		@SerialName("id")
-		val id: String? = null,
+    @SerialName("id")
+    val id: String? = null,
 
-		@SerialName("node_id")
-		val nodeId: String? = null
+    @SerialName("node_id")
+    val nodeId: String? = null
 )
